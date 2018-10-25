b04c988989b4075e6cbc068487e19016da8ff9ec
Artem <artemserdyuk321@gmail.com>
2018-10-25 06:53:41 +0300

    feat(app/todos)milestone25

--- src/app/core/interceptors/api-url.interceptor.ts
+++ src/app/core/interceptors/api-url.interceptor.ts
@@ -1,12 +1,13 @@
 import { HttpEvent, HttpHandler, HttpInterceptor,  HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import {environment} from '../../../environments/environment';
 
 @Injectable()
 export class ApiUrlInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const baseUrl = 'http://localhost:3000';
-    const apiReq = req.clone({ url: `${baseUrl}/${req.url}` });
+    const apiUrl = environment.apiUrl;
+    const apiReq = req.clone({ url: `${apiUrl}${req.url}` });
     return next.handle(apiReq);
 
   }
